984bd9b26c85fa2c0225b72a4dcb7596c033e35b
ardianajvazi <ardian_46@hotmail.com>
2015-10-30 13:44:14 -0700

    added color to questions

--- js/game.js
+++ js/game.js
@@ -14,11 +14,13 @@ var question1 = prompt('Did I live in California?');
 
   if (question1.toLowerCase() === 'yes' || question1.toLowerCase() ==='y') {
     res1.innerHTML = 'You are correct! I lived in California for 6 years!' + '<br><img width = "200px" src="img/ca_flag.png">';
+    res1.className = "green";
     score = score + 1;
     console.log('The user is correct! I lived in California for 6 years!');
 
   } else {
     res1.innerHTML = 'You are incorrect! I lived in California for 6 years!';
+    res1.className = "red";
     console.log('The user is incorrect! I lived in California for 6 years!');
   }
 }
@@ -28,12 +30,13 @@ var question2 = prompt('Did I live in Oregon?');
 
   if (question2.toLowerCase() === 'no' || question2.toLowerCase() ==='n') {
     res2.innerHTML = 'You are correct! I did not live in Oregon.' + '<br><img width = "200px" src="img/no_or.jpg">';
-
+    res2.className = "green";
     score = score + 1;
     console.log('The user is correct! I did not live in Oregon.');
 
   } else {
     res2.innerHTML = 'You are incorrect! I did not live in Oregon!';
+    res2.className = "red";
     console.log('The user is incorrect! I did not live in Oregon!');
   }
 }
@@ -43,11 +46,13 @@ var question3 = prompt('Did I live in Maryland?');
 
   if (question3.toLowerCase() === 'yes' || question3.toLowerCase() ==='y') {
     res3.innerHTML = 'You are correct! I lived in Maryland for 2 years!'+ '<br><img width = "200px" src="img/md_flag.gif">';
+    res3.className = "green";
     score = score + 1;
     console.log('The user is correct! I lived in Maryland for 2 years!');
 
   } else {
     res3.innerHTML = 'You are incorrect! I lived in Maryland for 2 years!';
+    res3.className = "red";
     console.log('The user is incorrect! I lived in Maryland for 2 years!');
   }
 }
@@ -62,16 +67,18 @@ var question4 = parseInt(prompt('How many cities did I live in, in California?')
 
   if (question4 === 3) {
     res4.innerHTML = 'You is correct!. I lived in San Rafael, Oakland and San Jose.' + '<br><img width = "200px" src="img/number_3.jpeg">';
-
+    res4.className = "green";
     score = score + 1;
     console.log('The user is correct!. I lived in San Rafael, Oakland and San Jose.');
 
   } else if (question4 > 3) {
     res4.innerHTML = 'You guessed too high! Hint: the number is between 1 and 5.';
+    res4.className = "red";
     console.log('The user guessed too high. Hint: the number is between 1 and 5.');
 
   } else {
     res4.innerHTML = 'You guessed too low! Hint: the number is between 1 and 5.';
+    res4.className = "red";
     console.log('The user guessed too low!. Hint: the number is between 1 and 5.');
   }
 }
@@ -81,12 +88,13 @@ var question5 = prompt('Did I attend Issaquah High School?');
 
   if (question5.toLowerCase() === 'yes' || question5.toLowerCase() ==='y') {
     res5.innerHTML = 'You are correct! I did attend Issaquah High School!' + '<br><img width = "200px" src="img/issaquah.jpg">';
-
+    res5.className = "green";
     score = score + 1;
     console.log('The user is correct! I did attend Issaquah High School!');
 
   } else {
     res5.innerHTML = 'You are incorrect! I did attend Issaquah High School!';
+    res5.className = "red";
     console.log('The user is incorrect! I did attend Issaquah High School!');
   }
 }
@@ -101,16 +109,18 @@ var question6 = parseInt(prompt('How many colleges did I attend in California?')
 
   if (question6 === 2) {
     res6.innerHTML = 'You are correct!. I attended Dominican University of California and Ex\'pression College for Digital Arts.' + '<br><img width = "200px" src="img/number_2.jpeg">';
-
+    res6.className = "green";
     score = score + 1;
     console.log('The user is correct!. I attended Dominican University of California and Ex\'pression College for Digital Arts.');
 
   } else if (question6 > 2) {
     res6.innerHTML = 'You guessed too high! Hint: the number is between 1 and 3.';
+    res6.className = "red";
     console.log('The user guessed too high. Hint: the number is between 1 and 3.');
 
   } else {
     res6.innerHTML = 'You guessed too low! Hint: the number is between 1 and 3.';
+    res6.className = "red";
     console.log('The user guessed too low!. Hint: the number is between 1 and 3.');
   }
 }

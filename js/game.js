b76a9d74bf61d71d46b4b880fecc02a83bd09363
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2015-11-27 07:12:42 -0800

    refactored code based off of Sam's class example.

--- js/game.js
+++ js/game.js
@@ -1,136 +1,44 @@
 
 var score = 0;
 var userName = prompt('Welcome to my guessing game! What is your name?');
+var qs = [['Did I live in California?', 'yes', 'y', 'You are correct! I lived in California for 6 years' + '<br><img width = "200px" src="Img/ca_flag.png">', 'green', 'You are incorrect! I lived in California for 6 years!', 'red' ], ['Did I live in Oregon?', 'no', 'n', 'You are correct! I did not live in Oregon.' + '<br><img width = "200px" src="Img/no_or.jpg">', 'green', 'You are incorrect! I did not live in Oregon!', 'red'], ['Did I live in Maryland?', 'yes', 'y', 'You are correct! I lived in Maryland for 2 years!'+ '<br><img width = "200px" src="Img/md_flag.gif">', 'green', 'You are incorrect! I lived in Maryland for 2 years!', 'red'], ['How many cities did I live in, in California?', 'three', '3', 'You are correct!. I lived in San Rafael, Oakland and San Jose.' + '<br><Img width = "200px" src="Img/number_3.jpeg">', 'green', 1, 'You are incorrect! I lived in 3 cities in California!', 'red' ], ['Did I attend Issaquah High School?', 'yes', 'y', 'You are correct! I did attend IHS' + '<br><img width = "200px" src="Img/issaquah.jpg">', 'green', 'You are incorrect! I did attend IHS', 'red'], ['How many colleges did I attend in California?', 'two', '2', 'You are correct!. I attended Dominican University of California and Ex\'pression College for Digital Arts.' + '<br><img width = "200px" src="Img/number_2.jpeg">', 'green', 'You are incorrect!. I attended two colleges', 'red']];
 
-var res1 = document.getElementById('response1');
-var res2 = document.getElementById('response2');
-var res3 = document.getElementById('response3');
-var res4 = document.getElementById('response4');
-var res5 = document.getElementById('response5');
-var res6 = document.getElementById('response6');
+  function displayQuestions(){
+    for (var i = 0; i < qs.length; i++) {
+      var nextQ = document.createElement('h2');
+      var nextA = document.createElement('p');
 
-function ques1() {
-var question1 = prompt('Did I live in California?');
+      nextQ.innerHTML = qs[i][0];
+      document.body.appendChild(nextQ);
 
-  if (question1.toLowerCase() === 'yes' || question1.toLowerCase() ==='y') {
-    res1.innerHTML = 'You are correct! I lived in California for 6 years!' + '<br><img width = "200px" src="Img/ca_flag.png">';
-    res1.className = "green";
-    score = score + 1;
-    console.log('The user is correct! I lived in California for 6 years!');
+      var user = prompt(qs[i][0]);
+      if (user.toLowerCase() === qs[i][1] || user.toLowerCase() === qs[i][2]) {
+        nextA.innerHTML = qs[i][3];
+        nextA.className = "green";
+        score++;
+        console.log('The user answered ' + user + ' the users score is ' + score);
+      } else {
+        nextA.innerHTML = qs[i][5];
+        nextA.className = "red";
+        console.log('The user answered ' + user + ' the correct answer was ' + qs[i][1]);
 
-  } else {
-    res1.innerHTML = 'You are incorrect! I lived in California for 6 years!';
-    res1.className = "red";
-    console.log('The user is incorrect! I lived in California for 6 years!');
-  }
-}
-
-function ques2() {
-var question2 = prompt('Did I live in Oregon?');
-
-  if (question2.toLowerCase() === 'no' || question2.toLowerCase() ==='n') {
-    res2.innerHTML = 'You are correct! I did not live in Oregon.' + '<br><img width = "200px" src="Img/no_or.jpg">';
-    res2.className = "green";
-    score = score + 1;
-    console.log('The user is correct! I did not live in Oregon.');
-
-  } else {
-    res2.innerHTML = 'You are incorrect! I did not live in Oregon!';
-    res2.className = "red";
-    console.log('The user is incorrect! I did not live in Oregon!');
-  }
-}
-
-function ques3() {
-var question3 = prompt('Did I live in Maryland?');
-
-  if (question3.toLowerCase() === 'yes' || question3.toLowerCase() ==='y') {
-    res3.innerHTML = 'You are correct! I lived in Maryland for 2 years!'+ '<br><img width = "200px" src="Img/md_flag.gif">';
-    res3.className = "green";
-    score = score + 1;
-    console.log('The user is correct! I lived in Maryland for 2 years!');
-
-  } else {
-    res3.innerHTML = 'You are incorrect! I lived in Maryland for 2 years!';
-    res3.className = "red";
-    console.log('The user is incorrect! I lived in Maryland for 2 years!');
-  }
-}
-
-function ques4() {
-var question4 = parseInt(prompt('How many cities did I live in, in California?'));
-
-  while (isNaN(question4)) {
-    alert('Please use numbers only to answer this question!');
-    question4 = parseInt(prompt('How many cities did I live in, in California?'));
+      }
+      document.body.appendChild(nextA);
+    }
   }
 
-  if (question4 === 3) {
-    res4.innerHTML = 'You is correct!. I lived in San Rafael, Oakland and San Jose.' + '<br><Img width = "200px" src="Img/number_3.jpeg">';
-    res4.className = "green";
-    score = score + 1;
-    console.log('The user is correct!. I lived in San Rafael, Oakland and San Jose.');
-
-  } else if (question4 > 3) {
-    res4.innerHTML = 'You guessed too high! Hint: the number is between 1 and 5.';
-    res4.className = "red";
-    console.log('The user guessed too high. Hint: the number is between 1 and 5.');
-
-  } else {
-    res4.innerHTML = 'You guessed too low! Hint: the number is between 1 and 5.';
-    res4.className = "red";
-    console.log('The user guessed too low!. Hint: the number is between 1 and 5.');
-  }
-}
-
-function ques5() {
-var question5 = prompt('Did I attend Issaquah High School?');
-
-  if (question5.toLowerCase() === 'yes' || question5.toLowerCase() ==='y') {
-    res5.innerHTML = 'You are correct! I did attend Issaquah High School!' + '<br><img width = "200px" src="Img/issaquah.jpg">';
-    res5.className = "green";
-    score = score + 1;
-    console.log('The user is correct! I did attend Issaquah High School!');
-
-  } else {
-    res5.innerHTML = 'You are incorrect! I did attend Issaquah High School!';
-    res5.className = "red";
-    console.log('The user is incorrect! I did attend Issaquah High School!');
-  }
-}
-
-function ques6(){
-var question6 = parseInt(prompt('How many colleges did I attend in California?'));
-
-  while (isNaN(question6)) {
-    alert('Please use numbers only to answer this question!');
-    question6 = parseInt(prompt('How many colleges did I attend in California?'));
-  }
-
-  if (question6 === 2) {
-    res6.innerHTML = 'You are correct!. I attended Dominican University of California and Ex\'pression College for Digital Arts.' + '<br><img width = "200px" src="Img/number_2.jpeg">';
-    res6.className = "green";
-    score = score + 1;
-    console.log('The user is correct!. I attended Dominican University of California and Ex\'pression College for Digital Arts.');
-
-  } else if (question6 > 2) {
-    res6.innerHTML = 'You guessed too high! Hint: the number is between 1 and 3.';
-    res6.className = "red";
-    console.log('The user guessed too high. Hint: the number is between 1 and 3.');
-
-  } else {
-    res6.innerHTML = 'You guessed too low! Hint: the number is between 1 and 3.';
-    res6.className = "red";
-    console.log('The user guessed too low!. Hint: the number is between 1 and 3.');
-  }
-}
-
-ques1();
-ques2();
-ques3();
-ques4();
-ques5();
-ques6();
+displayQuestions();
 
 alert(userName + ', you got ' + score + ' out of 6 correct!');
 console.log('The user got ' + score + ' out of 6 correct.');
+
+
+
+
+
+
+
+
+
+
+
